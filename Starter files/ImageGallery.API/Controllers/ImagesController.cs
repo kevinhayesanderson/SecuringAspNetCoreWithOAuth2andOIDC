74e593c918c9ba47aeb7b0140cd2202a059f3477
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-06-01 12:06:55 +0530

    Protecting the api's with roles

--- Starter files/ImageGallery.API/Controllers/ImagesController.cs
+++ Starter files/ImageGallery.API/Controllers/ImagesController.cs
@@ -54,6 +54,7 @@ public class ImagesController(IGalleryRepository galleryRepository, IWebHostEnvi
     }
 
     [HttpPost()]
+    [Authorize(Roles = "PayingUser")]
     public async Task<ActionResult<Image>> CreateImage([FromBody] ImageForCreation imageForCreation)
     {
         // Automapper maps only the Title in our configuration
@@ -81,6 +82,12 @@ public class ImagesController(IGalleryRepository galleryRepository, IWebHostEnvi
         // ownerId should be set - can't save image in starter solution, will
         // be fixed during the course
         //imageEntity.OwnerId = ...;
+        var ownerId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+        if (ownerId == null)
+        {
+            throw new Exception("User identifier is missing from the token.");
+        }
+        imageEntity.OwnerId = ownerId;
 
         // add and save.
         _galleryRepository.AddImage(imageEntity);
